5e73e85db2bda1291f41db94f0cee063c2cd5987
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2021-10-26 19:52:01 -0700

    Second commit added JS HTMLREADME.md added code in JS for CALC
    Co-Authored-by: Christen Weston <paleeserecycle@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,26 @@ const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
 alert(add(number1, number2));
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number) {
+    return number1 / number2;
+}
+
+//fahrenheit = C * 1.80 + 32
+//celcius = (f-32)/1.8
+
+function convertFToC(number1) {
+    return (number1 - 32)/ 1.8;
+}
+
+const fahrenheit = parseInt(prompt("Enter a temperature in Fahrenheit: "));
+
+alert(convertFToC(fahrenheit));
